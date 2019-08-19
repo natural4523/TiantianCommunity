790550d44f6a65f5a2157f2fa5a8ce699dc8b565
natural4523 <649158627@qq.com>
2019-08-19 14:29:55 +0800

    add related questions okkkkk

--- src/main/java/cn/edu/sdju/soft/community/service/QuestionService.java
+++ src/main/java/cn/edu/sdju/soft/community/service/QuestionService.java
@@ -10,13 +10,16 @@ import cn.edu.sdju.soft.community.mapper.UserMapper;
 import cn.edu.sdju.soft.community.model.Question;
 import cn.edu.sdju.soft.community.model.QuestionExample;
 import cn.edu.sdju.soft.community.model.User;
+import org.apache.commons.lang3.StringUtils;
 import org.apache.ibatis.session.RowBounds;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class QuestionService {
@@ -185,5 +188,23 @@ public class QuestionService {
         question.setViewCount(1);
         questionExtMapper.incView(question);
         //questionMapper.updateByExampleSelective(updateQuestion, questionExample);
+    }
+
+    public List<QuestionDTO> selectRelated(QuestionDTO queryDTO) {
+        if (StringUtils.isBlank(queryDTO.getTag())){
+            return new ArrayList<>();
+        }
+        String[] tags = StringUtils.split(queryDTO.getTag(), "，");
+        String regexpTag = Arrays.stream(tags).collect(Collectors.joining("|"));
+        Question question = new Question();
+        question.setId(queryDTO.getId());
+        question.setTag(regexpTag);
+        List<Question> questions = questionExtMapper.selectRelated(question);
+        List<QuestionDTO> questionDTOS = questions.stream().map(q -> {
+            QuestionDTO questionDTO = new QuestionDTO();
+            BeanUtils.copyProperties(q,questionDTO);
+            return questionDTO;
+        }).collect(Collectors.toList());
+        return questionDTOS;
     }
 }
